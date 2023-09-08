6f756c0128cd0bbd38b2a127660cae0fad8dec36
Kaede Aruga <arg@mokelab.com>
2023-09-08 19:05:21 +0900

    リアルタイムの時間を取得して表示する

--- lib/pages/date_time_page.dart
+++ lib/pages/date_time_page.dart
@@ -1,3 +1,5 @@
+import 'dart:async';
+
 import 'package:flutter/material.dart';
 import 'package:intl/intl.dart';
 
@@ -9,9 +11,21 @@ class DateTimePage extends StatefulWidget {
 }
 
 class _DateTimePageState extends State<DateTimePage> {
-  String nowTime = DateFormat('kk:mm').format(DateTime.now());
+  String nowTime = DateFormat('kk:mm:ss').format(DateTime.now());
 
   @override
+  void initState() {
+    super.initState();
+    Timer.periodic(const Duration(seconds: 1), _onTimer);
+  }
+
+  void _onTimer(Timer timer) {
+    var newTime = DateFormat('kk:mm:ss').format(DateTime.now());
+    setState(() {
+      nowTime = newTime;
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
