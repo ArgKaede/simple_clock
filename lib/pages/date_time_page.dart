475aeabef17cca9cbaf4371bad800127b1323500
Kaede Aruga <arg@mokelab.com>
2023-09-08 18:50:57 +0900

    create date time page

---
+++ lib/pages/date_time_page.dart
@@ -0,0 +1,22 @@
+import 'package:flutter/material.dart';
+
+class DateTimePage extends StatefulWidget {
+  const DateTimePage({super.key});
+
+  @override
+  State<DateTimePage> createState() => _DateTimePageState();
+}
+
+class _DateTimePageState extends State<DateTimePage> {
+  @override
+  Widget build(BuildContext context) {
+    return const Scaffold(
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [Text('00:00')],
+        ),
+      ),
+    );
+  }
+}
