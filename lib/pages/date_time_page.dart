352eeece46adb1801412821240ffe988fb46b7db
Kaede Aruga <arg@mokelab.com>
2023-09-13 16:30:01 +0900

    コード整理

--- lib/pages/date_time_page.dart
+++ lib/pages/date_time_page.dart
@@ -1,8 +1,6 @@
 import 'dart:async';
-
 import 'package:flutter/material.dart';
 import 'package:intl/intl.dart';
-
 import 'package:google_fonts/google_fonts.dart';
 
 class DateTimePage extends StatefulWidget {
@@ -13,8 +11,8 @@ class DateTimePage extends StatefulWidget {
 }
 
 class _DateTimePageState extends State<DateTimePage> {
-  bool isMenuVisible = false;
   // メニューの表示切り替え
+  bool isMenuVisible = false;
   void toggleMenu() {
     setState(() {
       isMenuVisible = !isMenuVisible;
@@ -22,12 +20,8 @@ class _DateTimePageState extends State<DateTimePage> {
     });
   }
 
-  // サイドバーのインデックス設定
-  int _selectedIndex = 0;
-
   // タイマー表示
   String nowTime = DateFormat('kk:mm:ss').format(DateTime.now());
-
   @override
   void initState() {
     super.initState();
@@ -41,72 +35,21 @@ class _DateTimePageState extends State<DateTimePage> {
     });
   }
 
-  final _pages = <Widget>[
-    Container(
-      color: Colors.red,
-      alignment: Alignment.center,
-      child: const Text('Page 1'),
-    ),
-    Container(
-      color: Colors.green,
-      alignment: Alignment.center,
-      child: const Text('Page 2'),
-    ),
-    Container(
-      color: Colors.blue,
-      alignment: Alignment.center,
-      child: const Text('Page 3'),
-    ),
-    Container(
-      color: Colors.yellow,
-      alignment: Alignment.center,
-      child: const Text('Page 4'),
-    ),
-    Container(
-      color: Colors.orange,
-      alignment: Alignment.center,
-      child: const Text('Page 5'),
-    ),
-  ];
-
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        // bottomNavigationBar: NavigationBar(
-        //   onDestinationSelected: (int index) {
-        //     setState(() {
-        //       _selectedIndex = index;
-        //     });
-        //   },
-        //   selectedIndex: _selectedIndex,
-        //   animationDuration: const Duration(seconds: 10),
-        //   elevation: 10,
-        //   height: 100,
-        //   labelBehavior: NavigationDestinationLabelBehavior.alwaysShow,
-        //   destinations: const <Widget>[
-        //     NavigationDestination(
-        //         icon: Icon(Icons.explore), label: 'ver A', tooltip: "tooltip1"),
-        //     NavigationDestination(
-        //         icon: Icon(Icons.commute), label: 'ver B', tooltip: "tooltip2"),
-        //     NavigationDestination(
-        //       icon: Icon(Icons.bookmark_border),
-        //       label: 'ver C',
-        //       tooltip: "tooltip3",
-        //       selectedIcon: Icon(Icons.bookmark),
-        //     ),
-        //     NavigationDestination(
-        //         icon: Icon(Icons.circle), label: 'ver D', tooltip: "tooltip4"),
-        //     NavigationDestination(
-        //         icon: Icon(Icons.train), label: 'ver E', tooltip: "tooltip5"),
-        //   ],
-        // ),
         body: GestureDetector(
             onTap: toggleMenu, // 画面をタップしたらメニューを切り替える
             child: Stack(children: [
               Center(
-                child: Text(
-                  'Clock',
-                  style: TextStyle(fontSize: 40.0),
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    Text(nowTime,
+                        style: GoogleFonts.robotoMono(
+                            textStyle: TextStyle(
+                                fontSize: 120, fontWeight: FontWeight.bold)))
+                  ],
                 ),
               ),
               AnimatedOpacity(
@@ -133,94 +76,6 @@ class _DateTimePageState extends State<DateTimePage> {
                   ),
                 ),
               ),
-            ])
-            //  Row(
-            //   children: [
-            //     MainContents(
-            //       index: _selectedIndex,
-            //       nowTime: nowTime,
-            //     ),
-            // NavigationRail(
-            //   destinations: const [
-            //     NavigationRailDestination(
-            //       icon: Icon(Icons.home),
-            //       label: Text('Home'),
-            //     ),
-            //     NavigationRailDestination(
-            //       icon: Icon(Icons.bookmark),
-            //       label: Text('Bookmark'),
-            //     ),
-            //     NavigationRailDestination(
-            //       icon: Icon(Icons.people),
-            //       label: Text('Friends'),
-            //     ),
-            //   ],
-            //   selectedIndex: _selectedIndex,
-            //   onDestinationSelected: (index) {
-            //     setState(() {
-            //       _selectedIndex = index;
-            //     });
-            //   },
-            // ),
-            // Center(
-            //   child: Column(
-            //     mainAxisAlignment: MainAxisAlignment.center,
-            //     children: [
-            //       Text(nowTime,
-            //           style: GoogleFonts.robotoMono(
-            //               textStyle: const TextStyle(
-            //                   fontSize: 120, fontWeight: FontWeight.bold)))
-            //     ],
-            //   ),
-            // ),
-            //   ],
-            // )
-            ));
-  }
-}
-
-// サイドバーに表示する画面サンプル
-class MainContents extends StatelessWidget {
-  const MainContents({super.key, required this.index, required this.nowTime});
-
-  final int index;
-  final String nowTime;
-
-  @override
-  Widget build(BuildContext context) {
-    switch (index) {
-      case 1:
-        return Expanded(
-          child: ColoredBox(
-            color: Colors.red[200]!,
-            child: const Center(
-              child: Text('Home'),
-            ),
-          ),
-        );
-      case 2:
-        return Expanded(
-          child: ColoredBox(
-            color: Colors.green[200]!,
-            child: const Center(
-              child: Text('Friends'),
-            ),
-          ),
-        );
-      default:
-        return Expanded(
-          child: Center(
-            child: Column(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: [
-                Text(nowTime,
-                    style: GoogleFonts.robotoMono(
-                        textStyle: const TextStyle(
-                            fontSize: 120, fontWeight: FontWeight.bold)))
-              ],
-            ),
-          ),
-        );
-    }
+            ])));
   }
 }
